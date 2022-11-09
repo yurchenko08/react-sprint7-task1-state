0b2eb9a2fa25b1f6d891d77d5389d30d88f41399
yurchenko08 <yurchenko08@gmail.com>
2022-11-09 19:23:33 +0200

    finished task1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 export default function App() {
-  return <div> React Marathon </div>;
+  const [text, setText] = useState("React Marathon");
+  return <div onClick={() => setText(text.toLowerCase())}> {text} </div>;
 }
-
